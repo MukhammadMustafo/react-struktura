272e93eb22ea894c9841b95fc56ee5d3ffbcc3ee
MukhammadMustafo <mukhammad20000@gmail.com>
2023-05-15 16:23:34 +0500

    asdf

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.scss';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+      <Footer/>
+
+      
+    </div>
+  );
+}
+
+export default App;
